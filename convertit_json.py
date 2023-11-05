811d2b4ce77ed9e990e1d2a99bd43353588865c0
EverVino <evervino00@gmail.com>
2023-11-05 14:30:16 -0400

    convert salud, transporte and salud to geojsondata

--- convertit_json.py
+++ convertit_json.py
@@ -1,6 +1,6 @@
 import json
 
-with open("transporte.json") as file:
+with open("salud.json") as file:
     json_object = json.load(file)
 
 features=[]
@@ -23,13 +23,17 @@ for element in json_object:
             "sistema": element["sistema"],
             "sub_sistema": element["sub_sistem"],
             "nivel": element["nivel"],
+            "uv": element["uv"],
+            "mz": element["mz"],
             "distrito": element["distrito"],
             "barrio": element["barrio"],
             "direccion": element["direccion"],
             "google_map": element["google_map"],
             "adm": element["adm"],
             "codigo": element["codigo"],
-            #"infraestructura": element["infraestru"],
+            "fecha_modificacion": element["last_edi_1"],
+            "infraestructura": element["infraestru"],
+            "adm_infraestructura": element["adm_infrae"]
         }
     }
 
@@ -40,30 +44,6 @@ geojson = {
     "features": features
 }
 
-with open("datos_deportes_scz.geojson", "w") as f:
+with open("datos_salud_scz.geojson", "w") as f:
     json.dump(geojson, f, indent=4, ensure_ascii=False)
 
-
-"""
-Ejemplo una feature dentro de un archivo geojson
- "features": [
-        {
-            "type": "Feature",
-            "id": "museos.21",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -68.13328259,
-                    -16.49520314
-                ]
-            },
-            "geometry_name": "geom",
-            "properties": {
-                "Nombre": "Academia Boliviana de Historia Militar",
-                "Direccion": "Pza. Murillo, Lado Cine Plaza",
-                "Zona": "Casco Viejo",
-                "distrito": 1
-            }
-        },
-    ]
-"""
